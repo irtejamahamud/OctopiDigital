d35eb6f75e39410806eb211f08fe4dc209a6e8b1
Irteja Mahamud <irtejamahamud9@gmail.com>
2025-06-29 15:02:00 +0600

    first commit

---
+++ Day-1/variable.js
@@ -0,0 +1,28 @@
+console.log("Varable:-----");
+var name = "irteja";
+console.log(name);
+//data types
+// 1. String   
+// 2. Number
+// 3. Boolean
+// 4. Null
+// 5. Undefined
+// 6. Object
+// 7. Symbol (ES6)
+ let age =20
+ let city = "kolkata";
+ let isMarried = false;
+ let nullValue = null;
+ console.log(age);
+ console.log(city); 
+console.log(isMarried);
+console.log(nullValue);
+// Object
+const student = {
+    name: "irtejaaObject",
+    age:20,
+    city: "kolkta"
+}
+
+console.log(student["name"]); // Accessing property using bracket notation
+console.log(student.name);
