df069ec8cc98f8003a0b5727bfa25022194cc6e2
Irteja Mahamud <irtejamahamud9@gmail.com>
2025-07-22 18:01:07 +0600

    update

--- LoginLogout/frontend/src/App.jsx
+++ LoginLogout/frontend/src/App.jsx
@@ -1,28 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   BrowserRouter as Router,
   Routes,
   Route,
   Navigate,
-  useNavigate,
 } from "react-router-dom";
 import Signup from "./Signup";
 import Login from "./Login";
 import Home from "./Home";
 
 function App() {
+  const [authChange, setAuthChange] = useState(0);
   const token = localStorage.getItem("token");
+
   return (
     <Router>
       <Routes>
-        <Route path="/signup" element={<Signup />} />
+        <Route
+          path="/signup"
+          element={<Signup onAuthChange={() => setAuthChange((c) => c + 1)} />}
+        />
         <Route
           path="/login"
-          element={token ? <Navigate to="/home" /> : <Login />}
+          element={
+            token ? (
+              <Navigate to="/home" />
+            ) : (
+              <Login onAuthChange={() => setAuthChange((c) => c + 1)} />
+            )
+          }
         />
         <Route
           path="/home"
-          element={token ? <Home /> : <Navigate to="/login" />}
+          element={
+            token ? (
+              <Home onAuthChange={() => setAuthChange((c) => c + 1)} />
+            ) : (
+              <Navigate to="/login" />
+            )
+          }
         />
         <Route
           path="*"
