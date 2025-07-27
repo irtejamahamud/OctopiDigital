12385cea8c83e06a4ee4a489c78eb67efc61b194
Irteja Mahamud <irtejamahamud9@gmail.com>
2025-07-27 23:38:56 +0600

    Updated FrontEnd

--- BlogFrontend/src/pages/HomePage.jsx
+++ BlogFrontend/src/pages/HomePage.jsx
@@ -4,7 +4,7 @@ export default function HomePage() {
   return (
     <div className="max-w-3xl mx-auto py-10 px-4">
       <h1 className="text-3xl font-bold mb-4 text-blue-700">
-        Welcome to the Blog Platform
+        Welcome to the Knowlage Hub
       </h1>
       <p className="mb-8 text-gray-700">
         Discover posts, share your thoughts, and connect with others.
